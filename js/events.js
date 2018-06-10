6f94ca3cd0d3ad352ddf40377f724f44bd642b98
Matthew Young <myoung162744@gmail.com>
2018-06-10 15:56:38 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,42 @@
 //define functions here
+function getIt() {
+  $('p').on('click', function() {
+    alert('Hey!')
+    return
+  })
+  return
+}
+
+function frameIt() {
+  $(document).on('load', function(){
+    $('img').addClass('tasty')
+  })
+  return
+}
+
+function pressIt() {
+  $('form').on('keydown', function(key){
+    if (key.which === 71) {
+      alert('g has been pressed')
+    }
+    return
+  })
+  return
+}
+
+function submitIt() {
+  $('form').on('submit',function(){
+    alert('Your form is going to be submitted now.')
+    return
+  })
+  return
+}
 
 $(document).ready(function(){
-
+  pressIt()
+  submitIt()
+  frameIt()
+  getIt()
 // call functions here
 
 });
