a67910e6047f9f471d6fad0da43fd753c791b1fa
Matthew Young <myoung162744@gmail.com>
2018-06-10 15:52:52 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -8,8 +8,8 @@ function getIt() {
 }
 
 function frameIt() {
-  $('img').on('load', function(){
-    .addClass('tasty')
+  $(document).on('load', function(){
+    $('.img').addClass('tasty')
     return
   })
   return
